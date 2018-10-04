9ff629d749d77fd9cb3cfc0aa0a4155379aab306
JobGetabu <getabujob@gmail.com>
2018-10-05 01:06:09 +0300

    validations on fields and injected AT methods

--- app/src/main/java/com/job/atdonate/ui/DonateFragment.java
+++ app/src/main/java/com/job/atdonate/ui/DonateFragment.java
@@ -1,6 +1,8 @@
 package com.job.atdonate.ui;
 
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.button.MaterialButton;
 import android.support.design.widget.BottomSheetDialogFragment;
@@ -12,9 +14,20 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.africastalking.AfricasTalking;
+import com.africastalking.models.airtime.AirtimeResponse;
+import com.africastalking.models.payment.checkout.CheckoutResponse;
+import com.africastalking.models.payment.checkout.MobileCheckoutRequest;
+import com.africastalking.models.sms.Recipient;
+import com.africastalking.services.AirtimeService;
+import com.africastalking.services.PaymentService;
+import com.africastalking.services.SmsService;
 import com.job.atdonate.R;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -80,6 +93,22 @@ public class DonateFragment extends BottomSheetDialogFragment {
         boolean valid = true;
 
         String am = contrAmountinput.getEditText().getText().toString();
+        String pn = dntPhoneNum.getEditText().getText().toString();
+
+        if (pn.isEmpty()) {
+            dntPhoneNum.setError("Phone is not valid");
+
+            dntP.setVisibility(View.GONE);
+            dntEImg.setVisibility(View.GONE);
+            dntPhoneNum.setVisibility(View.VISIBLE);
+
+            valid = false;
+        } else {
+            contrTextamount.setVisibility(View.VISIBLE);
+            contrEditImg.setVisibility(View.VISIBLE);
+            contrAmountinput.setVisibility(View.GONE);
+            contrAmountinput.setError(null);
+        }
 
         if (am.isEmpty() || am.equals("0")) {
             contrAmountinput.setError("Amount is not valid");
@@ -202,4 +231,136 @@ public class DonateFragment extends BottomSheetDialogFragment {
         contrEditImg.setVisibility(View.VISIBLE);
     }
 
+
+    //DO THIS LOGIC IN VIEWMODEL
+
+    /*
+   Implementation of our sendMessage method
+    */
+    private void sendMessage(final String number, final String message){
+
+        /*
+        get our sms service and use it to send the message
+         */
+        @SuppressLint("StaticFieldLeak")AsyncTask<Void, String, Void> smsTask = new AsyncTask<Void, String, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                /*
+                put it in try catch block
+                 */
+                try{
+
+                    //Log this
+                    Log.e("SMS INFO", "Attempting to send SMS");
+
+                    //get the sms service
+                    SmsService smsService = AfricasTalking.getSmsService();
+
+                    //Send the sms, get the response
+                    List<Recipient> recipients = smsService.send(message, new String[] {number});
+
+                    /*
+                    Log the response
+                     */
+                    Log.e("SMS RESPONSE", recipients.get(0).messageId + " " + recipients.get(0).status);
+                } catch (IOException e){
+
+                    Log.e("SMS FAILURE", e.toString());
+                }
+                return null;
+            }
+        };
+
+        smsTask.execute();
+    }
+
+    /*
+   Implementation of sendPayment()
+    */
+    private void sendPayment(final String number, final String amount){
+
+        /*
+        implement a demo checkout to this app
+         */
+        @SuppressLint("StaticFieldLeak") AsyncTask<Void, String, Void> paymentTask = new AsyncTask<Void, String, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                /*
+                same try catch block
+                 */
+                try{
+
+                    //Log it
+                    Log.e("PAYMENT ALERT","Trying to checkout");
+
+                    //get our payment service
+                    PaymentService paymentService = AfricasTalking.getPaymentService();
+
+                    //Create a checkout request
+                    MobileCheckoutRequest checkoutRequest = new MobileCheckoutRequest("Donation with AT","KES " + amount, number);
+
+                    //Initiate the checkout, using the checkoutrequest
+                    CheckoutResponse response = paymentService.checkout(checkoutRequest);
+
+                    //Log the response
+                    Log.e("PAYMENT RESPONSE", response.transactionId + " " + response.status + " " + response.description);
+
+                } catch (IOException e){
+
+                    Log.e("PAYMENT FAILURE","Failed to check out with exception " + e.toString());
+                }
+                return null;
+            }
+        };
+
+        //Execute our task
+        paymentTask.execute();
+    }
+
+    /*
+    implementation of sendAirtime()
+     */
+    private void sendAirtime(final HashMap<String, String> recipient, final String sendToName){
+
+        /*
+        Run our code in a separate thread from the UI thread, using AsyncTask. Required by Android for all Network calls
+         */
+
+        @SuppressLint("StaticFieldLeak") AsyncTask <Void, String, Void> taskSendAirtime = new AsyncTask<Void, String, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                /*
+                Where we put our code. This is the code that will be executed in the thread
+                 */
+                try {
+
+                /*
+                Log that we are trying to get service
+                 */
+                    Log.e("AIRTIME NOTICE", "Trying to get airtime service");
+                    AirtimeService service = AfricasTalking.getAirtimeService();
+
+                    //Now that we have the service, send the airtime, get the response
+                    AirtimeResponse response = service.send(recipient);
+
+                    //Log our success message
+                    Log.e("AIRTIME SUCCESS","Sent airtime worth " + response.totalAmount + " to " + sendToName);
+                } catch (IOException e){
+
+                    /*
+                    Log our failure
+                     */
+                    Log.e("AIRTIME FAILURE","Failed to send airtime with exception " + e.toString());
+                }
+
+                return null;
+            }
+        };
+
+        //Execute our task
+        taskSendAirtime.execute();
+    }
 }
--- app/src/main/java/com/job/atdonate/ui/MainActivity.java
+++ app/src/main/java/com/job/atdonate/ui/MainActivity.java
@@ -1,7 +1,7 @@
 package com.job.atdonate.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.job.atdonate.R;
 
