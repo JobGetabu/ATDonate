18502affc410a2ba335e41618b08dc83e2d40f9f
JobGetabu <getabujob@gmail.com>
2018-10-04 23:15:57 +0300

    theme change to MDC gradle upgrade added donation bottom dialogue

---
+++ app/src/main/java/com/job/atdonate/ui/DonateFragment.java
@@ -0,0 +1,7 @@
+package com.job.atdonate.ui;
+
+/**
+ * Created by Job on Thursday : 10/4/2018.
+ */
+public class DonateFragment {
+}
--- app/src/main/java/com/job/atdonate/ui/MainActivity.java
+++ app/src/main/java/com/job/atdonate/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.job.atdonate;
+package com.job.atdonate.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.job.atdonate.R;
 
 public class MainActivity extends AppCompatActivity {
 
