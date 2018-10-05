be3439154c0ecc0f06a6062e05f2c7fc0d891e73
JobGetabu <getabujob@gmail.com>
2018-10-05 05:54:10 +0300

    designed the main activity

--- app/src/main/java/com/job/atdonate/ui/MainActivity.java
+++ app/src/main/java/com/job/atdonate/ui/MainActivity.java
@@ -1,51 +1,40 @@
 package com.job.atdonate.ui;
 
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.widget.VideoView;
+import android.view.WindowManager;
 
 import com.job.atdonate.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.mVideoView)
-    VideoView mVideoView;
+    @BindView(R.id.mainbtn)
+    MaterialButton mainbtn;
     private DonateFragment donateFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         donateFragment = new DonateFragment();
 
+
+    }
+
+    @OnClick(R.id.mainbtn)
+    public void onViewClicked() {
         //call this on button donate in main screen
         donateFragment.show(getSupportFragmentManager(), DonateFragment.TAG);
-
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.clean);
-
-        mVideoView.setVideoURI(uri);
-        mVideoView.setSoundEffectsEnabled(false);
-        mVideoView.playSoundEffect(0);
-
-
-        mVideoView.start();
-
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setLooping(true);
-            }
-        });
     }
 }
